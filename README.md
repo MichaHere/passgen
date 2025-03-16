bc7850450ebb7eed0141f49f9f8ceef98a797ceb
Micha <micha@michahere.com>
2025-03-16 22:41:49 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# PassGen: A Simple Password Generator
+This is a simple password generator that allows the user to generate a password of any length and some specified character types. 
+
+## Dependencies
+* InquirerPy
+* Colorama
+* Python
+
+## Install
+To use PassGen, you can simply clone this GitHub repository to your computer, install the dependencies, and run the `passgen.py` file with Python. 
+
+Alternatively, if you want to use the script as a command in the Linux terminal, you can make a file in your `/usr/bin/` directory called `passgen`. Make sure you start the file with `#!` followed by the path to your Python installation. For example:
+
+    #!/usr/bin/python3
+
+After this line, simply paste in the content of the `passgen.py` file and run the command by typing the filename, in this case `passgen`.
---
+++ passgen.py
@@ -0,0 +1,81 @@
+from InquirerPy import inquirer
+from InquirerPy.base.control import Choice
+from InquirerPy.separator import Separator
+from colorama import Fore, Style
+from enum import Enum
+import random
+import string
+
+class CharacterTypes(Enum):
+    Lowercase = 1
+    Uppercase = 2
+    Number = 3
+    Special = 4
+
+def generate_password(password_length, character_types):
+    characters = {
+        CharacterTypes.Lowercase: string.ascii_lowercase,
+        CharacterTypes.Uppercase: string.ascii_uppercase,
+        CharacterTypes.Number: string.digits,
+        CharacterTypes.Special: string.punctuation,
+    }
+
+    chosen_characters = ""
+
+    for character_type in character_types:
+        chosen_characters += characters[character_type]
+    
+    generated_password = ""
+
+    for i in range(0, password_length):
+        random_index = random.randint(0, len(chosen_characters)-1)
+        generated_password += chosen_characters[random_index]
+    
+    return generated_password
+
+def prompt_length():
+    try:
+        return int(input("How Many Characters Do You Want Your Password To Be?\n~> "))
+    except ValueError:
+        print(f"{Fore.RED}ERROR: {Style.RESET_ALL}Please provide a valid number. ")
+        prompt_length()
+        return
+
+def prompt_character_types():
+    question_options = [Separator()] + [ 
+        Choice(option, name=f"{option.name} Characters", enabled=True) for option in list(CharacterTypes) 
+    ] + [Separator()]
+    
+    question = inquirer.checkbox(
+        message="Select Character Types:",
+        choices=question_options,
+        cycle=False,
+        transformer=lambda result: "%s Type%s Selected"
+        % (len(result), "s" if len(result) > 1 else ""),
+    )
+
+    return question.execute()
+
+def print_result(password):
+    print(
+f"""
+{Fore.YELLOW}\
+:_:_:_:_:_:_:_:_:_:_:_:_:_:_:_:_:_:_:
+{Style.RESET_ALL}
+Generated Password:
+{password}
+{Fore.YELLOW}
+:_:_:_:_:_:_:_:_:_:_:_:_:_:_:_:_:_:_:\
+{Style.RESET_ALL}\
+"""
+    )
+
+def main():
+    print (' '.join(sys.argv))
+    password_length = prompt_length()
+    character_types = prompt_character_types()
+    password = generate_password(password_length, character_types)
+    print_result(password)
+
+if (__name__ == "__main__"):
+    main()
